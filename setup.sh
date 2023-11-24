d030c10c33e99777f8476cd8b5631440c0751e32
rrkelley <rrkelley@UbuntuDesktopTemplate.desktoptemplate.r2klabs.com>
2023-11-24 10:34:37 -0500

    Finished setup.sh for complete installation

--- setup.sh
+++ setup.sh
@@ -9,16 +9,25 @@ echo "###############################"
 sudo apt update
 sudo apt upgrade -y
 
+echo "Install Java"
+echo "###############################"
+sudo apt install default-jdk -y
+
+
 echo "Install Apache Ant"
 echo "###############################"
-echo sudo apt install ant
+sudo apt install ant -y
+
+echo "Install VSCode"
+echo "##############################"
+echo sudo snap install --classic code
 
 echo "Download Antlr-4.13.1"
 echo "###############################"
 sudo wget http://www.antlr.org/download/antlr-4.13.1-complete.jar
 echo "Download ASM 9.6"
 echo "###############################"
-sudo wget https://repository.ow2.org/nexus/service/local/repositories/release/content/org/ow2/asm/asm/9.6/asm-6.jar
+sudo wget "https://repository.ow2.org/nexus/service/local/repositories/releases/content/org/ow2/asm/asm/9.6/asm-9.6.jar"
 
 echo "Move jar files to correct location"
 echo "###############################"
@@ -26,10 +35,20 @@ sudo mv *.jar /usr/local/lib
 
 echo "Set Environment Variables"
 echo "###############################"
-echo "JAVA_HOME
+echo "export JAVA_HOME=/usr/lib/jvm/default-java" >> /home/$USER/.bashrc
+echo "export PATH=$PATH:$JAVA_HOME/bin" >> /home/$USER/.bashrc
+echo "export CLASSPATH=$CLASSPATH:./:/usr/local/lib/antlr-4.13.1-complete.jar:/usr/local/lib/asm-9.6.jar" >> /home/$USER/.bashrc
+echo "export KC=/home/$USER/Documents/KnightCodeSkeleton" >> /home/$USER/.bashrc
 
+echo "Create Aliases"
+echo "###############################"
+echo "alias antlr4='java -jar /usr/local/lib/antlr-4.13.1-complete.jar'" >> /home/$USER/.bashrc
+echo "alias grun='java org.antlr.v4.gui.TestRig'" >> /home/$USER/.bashrc
 
-echo "export JAVA_HOME=/
+echo "Reload .bashrc"
+echo "###############################"
+echo `source /home/$USER/.bashrc`
+
 
 
 
